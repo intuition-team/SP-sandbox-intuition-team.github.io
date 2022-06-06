cef1338b6cd7c0e4dca4d92118e30d29d3f8e489
Maria Veres <veres.maria.dev@gmail.com>
2022-06-06 17:47:49 +0300

    footer-monday

--- src/scripts/scroll-link.js
+++ src/scripts/scroll-link.js
@@ -19,3 +19,57 @@ const ready = function() {
 };
 
 $(document).ready(ready);
+
+// parallax
+
+// $(window).scroll(function() {
+//   const 
+//     a = $(this).scrollTop(),
+//     b = 500,
+//     t = window.pageYOffset;
+  
+//   $(".parallax").css('transform', 'translate(-' + a / 1.6 + 'px, -' + 1 - a / b + 'px)');
+//     $(".one").css('bottom', -(t * 0.3) + 'px');
+//     $(".img-parallax").css('bottom', -(t * 0.1) + 'px');
+//     $(".three").css('bottom', -(t * 0.5) + 'px');
+//     $(".four").css('bottom', -(t * 0.6) + 'px');
+//     $(".five").css('bottom',-(t * 0.7) + 'px');
+  
+// });
+
+// $(".header-parallax").on('mousemove', (e) => {
+//   const cx = $(window).width() / 2,
+//         cy = $(window).height() / 4,
+//         x = ((cx - e.pageX) / cx) * 1,
+//         y = ((cy - e.pageY) / cy) * 4;
+//   $(".img-parallax").css('transform',`translate(${x}px, ${y}px)`);
+  
+// });
+
+
+// parallax2
+
+$(window).scroll(function() {
+  const 
+    a = $(this).scrollTop(),
+    b = 500,
+    t = window.pageYOffset;
+  
+  $(".parallax").css('transform', 'translate(-' + a / 1.6 + 'px, -' + 1 - a / b + 'px)');
+    $(".one").css('bottom', -(t * 23) + 'px');
+    $(".img-parallax").css('bottom', -(t * 31) + 'px');
+    $(".three").css('bottom', -(t * 15) + 'px');
+    $(".four").css('bottom', -(t * 16) + 'px');
+    $(".five").css('bottom',-(t * 17) + 'px');
+  
+});
+
+$(".header-parallax").on('mousemove', (e) => {
+  const cx = $(window).width() / 2,
+        cy = $(window).height() / 4,
+        x = ((cx - e.pageX) / cx) * 1,
+        y = ((cy - e.pageY) / cy) * 24;
+  $(".img-parallax").css('transform',`translate(${x}px, ${y}px)`);
+  
+});
+
